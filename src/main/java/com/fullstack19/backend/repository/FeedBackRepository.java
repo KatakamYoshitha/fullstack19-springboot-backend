c7d04f2d60274c4a159fd17470fb67982fcc9ae0
KatakamYoshitha <yoshithaka123@gmail.com>
2026-04-28 01:35:43 +0530

    Rename FeedbackRepository to FeedBackRepository

--- src/main/java/com/fullstack19/backend/repository/FeedBackRepository.java
+++ src/main/java/com/fullstack19/backend/repository/FeedBackRepository.java
@@ -1,9 +1,9 @@
 package com.fullstack19.backend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.fullstack19.backend.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
+public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
 }
