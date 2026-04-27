7c40e82c245efe21d18344e6f42a825a2004646b
KatakamYoshitha <yoshithaka123@gmail.com>
2026-04-28 01:36:02 +0530

    Refactor ProgramController to use programRepository

--- src/main/java/com/fullstack19/backend/controller/ProgramController.java
+++ src/main/java/com/fullstack19/backend/controller/ProgramController.java
@@ -1,20 +1,26 @@
 package com.fullstack19.backend.controller;
 
+import com.fullstack19.backend.model.Program;
+import com.fullstack19.backend.repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/programs")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class ProgramController {
 
     @Autowired
-    ProgramRepository repo;
+    private ProgramRepository programRepository;
 
     @GetMapping
-    public List<Program> getPrograms(){
-        return repo.findAll();
+    public List<Program> getAllPrograms() {
+        return programRepository.findAll();
     }
 
     @PostMapping
-    public Program addProgram(@RequestBody Program program){
-        return repo.save(program);
+    public Program addProgram(@RequestBody Program program) {
+        return programRepository.save(program);
     }
 }
