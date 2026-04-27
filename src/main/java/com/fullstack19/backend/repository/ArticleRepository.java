e7bcf229bfe461eb558d3e545c6a1ff60f6789fc
KatakamYoshitha <yoshithaka123@gmail.com>
2026-04-28 01:34:14 +0530

    Add JpaRepository and Repository annotations

--- src/main/java/com/fullstack19/backend/repository/ArticleRepository.java
+++ src/main/java/com/fullstack19/backend/repository/ArticleRepository.java
@@ -1,8 +1,9 @@
 package com.fullstack19.backend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.fullstack19.backend.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
 }
