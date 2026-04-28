5ffa677f738eddb924a449cb5fe13ca499d7de95
KatakamYoshitha <yoshithaka123@gmail.com>
2026-04-28 11:55:38 +0530

    Update FeedBackRepository.java

--- src/main/java/com/fullstack19/backend/repository/FeedBackRepository.java
+++ src/main/java/com/fullstack19/backend/repository/FeedBackRepository.java
@@ -1,7 +1,9 @@
 package com.fullstack19.backend.repository;
 
+import com.fullstack19.backend.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.fullstack19.backend.model.FeedBack;
+import org.springframework.stereotype.Repository;
 
-public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 }
